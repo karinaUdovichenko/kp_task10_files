8cdd3678cdb249775bdeb9534458056d425a16af
karinaUdovichenko <113229455+karinaUdovichenko@users.noreply.github.com>
2023-05-18 17:59:20 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CountLinesButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CreateFile = new System.Windows.Forms.Button();
+            this.ReviewText = new System.Windows.Forms.Button();
+            this.CountConditionButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.CountWordButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ReplaceButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.ToUpperButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(41, 141);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(158, 46);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Записать в файл текст";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(128, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Введите название файла";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите текст для записи в файл";
+            // 
+            // CountLinesButton
+            // 
+            this.CountLinesButton.Location = new System.Drawing.Point(503, 12);
+            this.CountLinesButton.Name = "CountLinesButton";
+            this.CountLinesButton.Size = new System.Drawing.Size(158, 46);
+            this.CountLinesButton.TabIndex = 9;
+            this.CountLinesButton.Text = "Вывести общее кол-во строк в файле";
+            this.CountLinesButton.UseVisualStyleBackColor = true;
+            this.CountLinesButton.Click += new System.EventHandler(this.CountLinesButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(245, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 10;
+            // 
+            // CreateFile
+            // 
+            this.CreateFile.Location = new System.Drawing.Point(45, 54);
+            this.CreateFile.Name = "CreateFile";
+            this.CreateFile.Size = new System.Drawing.Size(158, 30);
+            this.CreateFile.TabIndex = 11;
+            this.CreateFile.Text = "Создавать файл";
+            this.CreateFile.UseVisualStyleBackColor = true;
+            this.CreateFile.Click += new System.EventHandler(this.CreateFile_Click);
+            // 
+            // ReviewText
+            // 
+            this.ReviewText.Location = new System.Drawing.Point(41, 193);
+            this.ReviewText.Name = "ReviewText";
+            this.ReviewText.Size = new System.Drawing.Size(158, 45);
+            this.ReviewText.TabIndex = 13;
+            this.ReviewText.Text = "Вывести из файла текст";
+            this.ReviewText.UseVisualStyleBackColor = true;
+            this.ReviewText.Click += new System.EventHandler(this.ReviewText_Click);
+            // 
+            // CountConditionButton
+            // 
+            this.CountConditionButton.Location = new System.Drawing.Point(503, 64);
+            this.CountConditionButton.Name = "CountConditionButton";
+            this.CountConditionButton.Size = new System.Drawing.Size(158, 46);
+            this.CountConditionButton.TabIndex = 14;
+            this.CountConditionButton.Text = "Вывести общее кол-во строк в файле";
+            this.CountConditionButton.UseVisualStyleBackColor = true;
+            this.CountConditionButton.Click += new System.EventHandler(this.CountConditionButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(245, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 18);
+            this.label4.TabIndex = 11;
+            // 
+            // CountWordButton
+            // 
+            this.CountWordButton.Location = new System.Drawing.Point(503, 115);
+            this.CountWordButton.Name = "CountWordButton";
+            this.CountWordButton.Size = new System.Drawing.Size(158, 54);
+            this.CountWordButton.TabIndex = 15;
+            this.CountWordButton.Text = "Подсчет частоты появления заданного символа/слова";
+            this.CountWordButton.UseVisualStyleBackColor = true;
+            this.CountWordButton.Click += new System.EventHandler(this.CountWordButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(245, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 18);
+            this.label5.TabIndex = 16;
+            // 
+            // ReplaceButton
+            // 
+            this.ReplaceButton.Location = new System.Drawing.Point(41, 327);
+            this.ReplaceButton.Name = "ReplaceButton";
+            this.ReplaceButton.Size = new System.Drawing.Size(158, 54);
+            this.ReplaceButton.TabIndex = 17;
+            this.ReplaceButton.Text = "Заменить заданное слова/символа другим словом/символом";
+            this.ReplaceButton.UseVisualStyleBackColor = true;
+            this.ReplaceButton.Click += new System.EventHandler(this.ReplaceButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(54, 261);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(128, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Введите слово которое хотите заменить";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 285);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Введите заменяемое слово";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(54, 301);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 20);
+            this.textBox4.TabIndex = 20;
+            // 
+            // ToUpperButton
+            // 
+            this.ToUpperButton.Location = new System.Drawing.Point(601, 326);
+            this.ToUpperButton.Name = "ToUpperButton";
+            this.ToUpperButton.Size = new System.Drawing.Size(92, 49);
+            this.ToUpperButton.TabIndex = 23;
+            this.ToUpperButton.Text = "Приведение текста";
+            this.ToUpperButton.UseVisualStyleBackColor = true;
+            this.ToUpperButton.Click += new System.EventHandler(this.ToUpperButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(461, 228);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(101, 85);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 46);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 28;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Слово";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 17);
+            this.radioButton1.TabIndex = 27;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Весь текст";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(467, 344);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(116, 20);
+            this.textBox5.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(453, 327);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(142, 13);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Введите слово для поиска";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Location = new System.Drawing.Point(568, 228);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(122, 85);
+            this.groupBox2.TabIndex = 29;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 46);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(108, 17);
+            this.radioButton5.TabIndex = 28;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Нижний регистр";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 24);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(116, 17);
+            this.radioButton6.TabIndex = 27;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Вверхний регистр";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 389);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ToUpperButton);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.ReplaceButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CountWordButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CountConditionButton);
+            this.Controls.Add(this.ReviewText);
+            this.Controls.Add(this.CreateFile);
+            this.Controls.Add(this.CountLinesButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CountLinesButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CreateFile;
+        private System.Windows.Forms.Button ReviewText;
+        private System.Windows.Forms.Button CountConditionButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button CountWordButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button ReplaceButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button ToUpperButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Путь к файлу
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+            textBox1.Text += Environment.NewLine;
+            // Получаем текст из TextBox и преобразуем его в массив байтов
+            byte[] dataToWrite = Encoding.UTF8.GetBytes(textBox1.Text);
+
+            // Записываем данные в файл
+            using (FileStream fs = new FileStream(filePath, FileMode.Append))
+            {
+                fs.Write(dataToWrite, 0, dataToWrite.Length);
+            }
+            textBox1.Text = " ";
+        }
+        private void ReviewText_Click(object sender, EventArgs e)
+        {
+            // Путь к файлу
+
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+
+            // Считываем данные из файла
+            byte[] dataFromFile = File.ReadAllBytes(filePath);
+
+            // Преобразуем массив байтов в строку и отображаем ее в TextBox
+            textBox1.Text = Encoding.UTF8.GetString(dataFromFile);
+            label3.Text = "Полученный текст из файла";
+        }
+        public string fileNameWrite()
+        {
+            string fileName = textBox2.Text;
+            return fileName;
+        }
+        private void CreateFile_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(fileNameWrite()))
+            {
+                MessageBox.Show($"Файл с именем {fileNameWrite()} уже существует", "Ошибка");
+            }
+            else {
+                using (FileStream fs = new FileStream(fileNameWrite(), FileMode.CreateNew)) ;
+            }
+        }
+
+        private void CountLinesButton_Click(object sender, EventArgs e)
+        {
+            // Путь к файлу
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+
+            // Считываем все строки из файла в массив строк
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Выводим количество строк в Label
+            label1.Text = $"Количество строк: {lines.Length}";
+        }
+
+        private void CountConditionButton_Click(object sender, EventArgs e)
+        {
+            // Путь к файлу
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+
+            // Считываем все строки из файла в массив строк
+            string[] lines = File.ReadAllLines(filePath);
+
+            // Считаем количество строк, содержащих заданное условие
+            int count = lines.Count(line => line.Contains("a"));
+
+            // Выводим количество строк в Label
+            label4.Text = $"Количество строк \nс условием: {count}";
+        }
+
+        private void CountWordButton_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+            char charToCount = 'a'; // символ, частоту появления которого мы хотим подсчитать
+            int charCount = 0;
+
+            // Читаем данные из файла и подсчитываем частоту появления символа
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    foreach (char c in line)
+                    {
+                        if (c == charToCount)
+                        {
+                            charCount++;
+                        }
+                    }
+                }
+            }
+            label5.Text = $"Количество: {charCount}";
+        }
+
+        private void ReplaceButton_Click(object sender, EventArgs e)
+        {
+            // Путь к файлу
+            string oldWord = textBox3.Text;
+            string newWord = textBox4.Text;
+            string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+            // Считываем текст из файла в строку
+            string text = File.ReadAllText(filePath);
+
+            // Заменяем все вхождения заданного слова на новое слово
+            string newText = text.Replace(oldWord, newWord);
+
+            // Преобразуем новый текст в массив байтов и записываем его в файл
+            byte[] dataToWrite = Encoding.UTF8.GetBytes(newText);
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            {
+                fs.Write(dataToWrite, 0, dataToWrite.Length);
+            }
+        }
+
+        private void ToUpperButton_Click(object sender, EventArgs e)
+        {
+            
+            if (radioButton1.Checked)
+            {
+                string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+                string text = File.ReadAllText(filePath);
+                if (radioButton6.Checked)
+                {
+                    // Приводим текст к верхнему регистру
+                    string newText = text.ToUpper();
+                    byte[] dataToWrite = Encoding.UTF8.GetBytes(newText);
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                    {
+                        fs.Write(dataToWrite, 0, dataToWrite.Length);
+                    }
+                }
+                if (radioButton5.Checked)
+                {
+                    // Приводим текст к нижнему регистру
+                    string newText = text.ToLower();
+                    byte[] dataToWrite = Encoding.UTF8.GetBytes(newText);
+                    using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                    {
+                        fs.Write(dataToWrite, 0, dataToWrite.Length);
+                    }
+                }
+            }
+            if (radioButton2.Checked)
+            {
+                
+                string filePath = @"C:\Users\polla\Desktop\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\" + fileNameWrite();
+                string text = File.ReadAllText(filePath);
+                string word = textBox5.Text;
+                if (radioButton6.Checked)
+                { 
+                    string fileContent;
+                    // Читаем содержимое файла в строку
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        fileContent = sr.ReadToEnd();
+                    }
+                    if (fileContent.Contains(word))
+                    {
+                        string newWord = text.Replace(word, word.ToUpper());
+                        byte[] dataToWrite = Encoding.UTF8.GetBytes(newWord);
+                        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                        {
+                            fs.Write(dataToWrite, 0, dataToWrite.Length);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Слово '{word}' не найдено в файле", "Результат");
+                    }
+                }
+                if (radioButton5.Checked)
+                {
+                    string fileContent;
+                    // Читаем содержимое файла в строку
+                    using (StreamReader sr = new StreamReader(filePath))
+                    {
+                        fileContent = sr.ReadToEnd();
+                    }
+                    if (fileContent.Contains(word))
+                    {
+                        string newWord = text.Replace(word, word.ToLower());
+                        byte[] dataToWrite = Encoding.UTF8.GetBytes(newWord);
+                        using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                        {
+                            fs.Write(dataToWrite, 0, dataToWrite.Length);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Слово '{word}' не найдено в файле", "Результат");
+                    }
+                }
+
+            }
+        }
+    }
+}
